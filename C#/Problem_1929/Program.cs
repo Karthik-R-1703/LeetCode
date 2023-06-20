b76800bf412f19ecdb6045d7ff984fe789bb84c7
Karthik R <karthik.rcg@zohocorp.com>
2023-06-20 19:47:09 +0530

    Added Problem 1929

---
+++ C#/Problem_1929/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Problem_1929
+{
+    // 1929. Concatenation of Array
+    // https://leetcode.com/problems/concatenation-of-array/
+    internal class Program
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            int[] result = new int[2 * nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] = nums[i];
+                result[i + nums.Length] = nums[i];
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}

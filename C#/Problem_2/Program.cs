8c0f8a849f658a13c2909a6a1c08a4c266d66f55
Karthik R <karthik.rcg@zohocorp.com>
2023-06-21 17:05:11 +0530

    Partial Problem_2 & Problem_2448

--- C#/Problem_2/Program.cs
+++ C#/Problem_2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Problem_2
+﻿using System.Net.NetworkInformation;
+
+namespace Problem_2
 {
     //Definition for singly-linked list.
     public class ListNode
@@ -18,9 +20,57 @@
     {
         public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            ListNode sum = null;
+            int sum = Convert_List_to_Num(l1) + Convert_List_to_Num(l2);
 
-            return sum;
+            return Convert_Num_to_List(sum);
+        }
+
+        public static int Convert_List_to_Num(ListNode l)
+        {
+            int val = 0;
+
+            while (l != null)
+            {
+                val = (val * 10) + l.val;
+                l = l.next;
+            }
+
+            int revVal = 0;
+
+            while (val > 0)
+            {
+                int r = val % 10;
+                revVal = (revVal * 10) + r;
+                val /= 10;
+            }
+
+            return revVal;
+        }
+
+        public static ListNode Convert_Num_to_List(int val)
+        {
+            ListNode l = new ListNode();
+            ListNode head = l;
+
+            while (val > 0)
+            {
+                int num = val % 10;
+                val /= 10;
+
+                head.val = num;
+                if (val / 10 > 0)
+                {
+                    head.next = new ListNode();
+                }
+                else
+                {
+                    head.next = null;
+                }
+
+                head = head.next;
+            }
+
+            return l;
         }
 
         static void Main(string[] args)
---
+++ C#/Problem_2448/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Problem_2448
+{
+    // 2448. Minimum Cost to Make Array Equal
+    // https://leetcode.com/problems/minimum-cost-to-make-array-equal/
+    internal class Program
+    {
+        public static long MinCost(int[] nums, int[] cost)
+        {
+            long minCost = long.MaxValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                long average = nums[i];
+                long tempCost = 0;
+
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    long c = average - nums[j];
+                    tempCost += (Math.Abs(c) * cost[j]);
+
+                    if (tempCost > minCost)
+                    {
+                        break;
+                    }
+                }
+
+                if (tempCost < minCost)
+                {
+                    minCost = tempCost;
+                }
+            }
+
+            return minCost;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 735103, 366367, 132236, 133334, 808160, 113001, 49051, 735598, 686615, 665317, 999793, 426087, 587000, 649989, 509946, 743518 };
+            int[] cost = { 724182, 447415, 723725, 902336, 600863, 287644, 13836, 665183, 448859, 917248, 397790, 898215, 790754, 320604, 468575, 825614 };
+            long exp = 1907611126748;
+            long minCost = MinCost(nums, cost);
+        }
+    }
+}

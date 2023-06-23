85b1272396b88e39524be5fed22d6cc3edd35536
Karthik R <karthik.rcg@zohocorp.com>
2023-06-23 16:24:29 +0530

    Added Problem 1295

---
+++ C#/Problem_1295/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Problem_1295
+{
+    // 1295. Find Numbers with Even Number of Digits
+    // https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+    internal class Program
+    {
+        public static int FindNumbers(int[] nums)
+        {
+            int count = 0;
+
+            foreach (int num in nums)
+            {
+                if (IsEvenNumDigits(num))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static bool IsEvenNumDigits(int num)
+        {
+            int count = 0;
+            while (num > 0)
+            {
+                count++;
+                num /= 10;
+            }
+
+            return count % 2 == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 12, 345, 2, 6, 7896 };
+            FindNumbers(nums1);
+
+            int[] nums2 = { 555, 901, 482, 1771 };
+            FindNumbers(nums2);
+        }
+    }
+}

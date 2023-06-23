831f9ac8947b9572dea249abd648971569d7417d
Karthik R <karthik.rcg@zohocorp.com>
2023-06-23 23:38:38 +0530

    Empty add Problem 1089

---
+++ C#/Problem_1089/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1089
+{
+    // 1089. Duplicate Zeros
+    // https://leetcode.com/problems/duplicate-zeros/
+    internal class Program
+    {
+        public void DuplicateZeros(int[] arr)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}

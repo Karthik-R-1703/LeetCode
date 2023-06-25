a03e8a5aee965510637e9f1663a0006a66872a96
Karthik <karthikr.1703@gmail.com>
2023-06-26 00:19:59 +0530

    Empty problem 27 added

---
+++ C#/Problem_27/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Problem_27
+{
+    // 27. Remove Element
+    // https://leetcode.com/problems/remove-element/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 3, 2, 2, 3 };
+            RemoveElement(nums1, 3);
+
+            int[] nums2 = { 0, 1, 2, 2, 3, 0, 4, 2 };
+            RemoveElement(nums2, 2);
+        }
+
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int k = 0;
+
+            return k;
+        }
+    }
+}

93e514c9dc2b75f69fdab3982564514d8dfd8c4d
Karthik R <karthik.rcg@zohocorp.com>
2023-06-30 16:37:44 +0530

    Added Problem 1051

---
+++ C#/Problem_1051/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Problem_1051
+{
+    // 1051. Height Checker
+    // https://leetcode.com/problems/height-checker/
+    internal class Program
+    {
+        public static int HeightChecker(int[] heights)
+        {
+            int[] expected = (int[])heights.Clone();
+            int notMatch = 0;
+
+            Array.Sort(expected);
+
+            for (int i = 0; i < heights.Length; i++)
+            {
+                if (heights[i] != expected[i])
+                {
+                    notMatch++;
+                }
+            }
+
+            return notMatch;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}

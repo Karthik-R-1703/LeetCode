1b281eac3fd0a1ff6ac434c257dd3886459cb56d
Karthik R <karthik.rcg@zohocorp.com>
2023-06-30 16:12:07 +0530

    Added Problem 905

---
+++ C#/Problem_905/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Problem_905
+{
+    // 905. Sort Array By Parity
+    // https://leetcode.com/problems/sort-array-by-parity/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums;
+
+            nums = new List<int>() { 3, 1, 2, 4 };
+            SortArrayByParity(nums.ToArray());
+
+            nums = new List<int>() { 0 };
+            SortArrayByParity(nums.ToArray());
+        }
+
+        public static int[] SortArrayByParity(int[] nums)
+        {
+            int[] sortedArray = new int[nums.Length];
+            int left = 0;
+            int right = nums.Length - 1;
+
+            foreach (int num in nums)
+            {
+                if (num % 2 == 0)
+                {
+                    sortedArray[left] = num;
+                    left++;
+                }
+                else
+                {
+                    sortedArray[right] = num;
+                    right--;
+                }
+            }
+
+            return sortedArray;
+        }
+    }
+}

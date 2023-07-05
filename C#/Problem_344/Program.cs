fb9df9f77b34be50dc6ef3ecc1be7fe33a26b186
Karthik R <karthik.rcg@zohocorp.com>
2023-07-05 13:27:41 +0530

    Added Problem 344

---
+++ C#/Problem_344/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Problem_344
+{
+    // 344. Reverse String
+    // https://leetcode.com/problems/reverse-string/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s1 = { 'h', 'e', 'l', 'l', 'o' };
+            ReverseString(s1);
+
+            char[] s2 = { 'H', 'a', 'n', 'n', 'a', 'h' };
+            ReverseString(s2);
+        }
+
+        public static void ReverseString(char[] s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+            while (i < j)
+            {
+                char c = s[i];
+                s[i] = s[j];
+                s[j] = c;
+
+                i++;
+                j--;
+            }
+        }
+    }
+}
---
+++ C#/Problem_561/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Problem_561
+{
+    // 561. Array Partition
+    // https://leetcode.com/problems/array-partition/
+    internal class Program
+    {
+        public static int ArrayPairSum(int[] nums)
+        {
+            int sum = 0;
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 1, 4, 3, 2 };
+            ArrayPairSum(nums1);
+
+            int[] nums2 = { 6, 2, 6, 5, 1, 2 };
+            ArrayPairSum(nums2);
+        }
+    }
+}

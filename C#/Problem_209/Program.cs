c1ab493d6aecdea84a14323853fc2e236646a12e
Karthik R <karthik.rcg@zohocorp.com>
2023-07-06 10:14:15 +0530

    added Problem 209

---
+++ C#/Problem_209/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_209
+{
+    // 209. Minimum Size Subarray Sum
+    // https://leetcode.com/problems/minimum-size-subarray-sum/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 2, 3, 1, 2, 4, 3 };
+            MinSubArrayLen(7, nums1);
+
+            int[] nums2 = { 1, 4, 4 };
+            MinSubArrayLen(4, nums2);
+
+            int[] nums3 = { 1, 1, 1, 1, 1, 1, 1, 1 };
+            MinSubArrayLen(11, nums3);
+        }
+
+        public static int MinSubArrayLen(int target, int[] nums)
+        {
+            int left = 0;
+            int right = 0;
+            int sumOfCurrentWindow = 0;
+            int res = int.MaxValue;
+
+            for (right = 0; right < nums.Length; right++)
+            {
+                sumOfCurrentWindow += nums[right];
+
+                while (sumOfCurrentWindow >= target)
+                {
+                    res = Math.Min(res, right - left + 1);
+                    sumOfCurrentWindow -= nums[left++];
+                }
+            }
+
+            if (res == int.MaxValue)
+                res = 0;
+
+            return res;
+        }
+    }
+}

ca4f68ae542c367b21f05c1ee2695fe9c2511827
Karthik R <karthik.rcg@zohocorp.com>
2023-07-07 10:08:06 +0530

    Added Problem 151

---
+++ C#/Problem_151/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem_151
+{
+    // 151. Reverse Words in a String
+    // https://leetcode.com/problems/reverse-words-in-a-string/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string ReverseWords(string s)
+        {
+            return string.Join(" ", s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Reverse());
+        }
+    }
+}

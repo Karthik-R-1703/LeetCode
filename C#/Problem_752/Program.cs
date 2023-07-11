b994a3fc2d65eeac1f725f42273b9e7874a60b55
Karthik R <karthik.rcg@zohocorp.com>
2023-07-11 23:36:17 +0530

    Empty Problem 752 added

---
+++ C#/Problem_752/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Problem_752
+{
+    // 752. Open the Lock
+    // https://leetcode.com/problems/open-the-lock/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] deadends1 = { "0201", "0101", "0102", "1212", "2002" };
+            OpenLock(deadends1, "0202");
+
+            string[] deadends2 = { "8888" };
+            OpenLock(deadends2, "0009");
+
+            string[] deadends3 = { "8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888" };
+            OpenLock(deadends3, "8888");
+        }
+
+        public static int OpenLock(string[] deadends, string target)
+        {
+            int turns = 0;
+            return turns;
+        }
+    }
+}

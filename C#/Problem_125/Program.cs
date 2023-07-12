5f756b8aa41e3f89f86036f93b6631c8532b42f4
Karthik R <karthik.rcg@zohocorp.com>
2023-07-12 15:14:34 +0530

    Added Problem 125 & 680

---
+++ C#/Problem_125/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Problem_125
+{
+    // 125. Valid Palindrome
+    // https://leetcode.com/problems/valid-palindrome/
+    internal class Program
+    {
+        public static bool IsPalindrome(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+            Regex regex = new Regex("^[a-zA-Z0-9]+$");
+
+            while (left < right)
+            {
+                while (left < s.Length && !regex.IsMatch(s[left].ToString().ToUpperInvariant()))
+                {
+                    left++;
+                }
+
+                while (right >= 0 && !regex.IsMatch(s[right].ToString().ToUpperInvariant()))
+                {
+                    right--;
+                }
+
+                if (left < s.Length && right >= 0)
+                {
+                    if (s[left].ToString().ToUpperInvariant() != s[right].ToString().ToUpperInvariant())
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        left++;
+                        right--;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            IsPalindrome("A man, a plan, a canal: Panama");
+            IsPalindrome("race a car");
+            IsPalindrome(" ");
+        }
+    }
+}
---
+++ C#/Problem_680/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Problem_680
+{
+    // 680. Valid Palindrome II
+    // https://leetcode.com/problems/valid-palindrome-ii/
+    internal class Program
+    {
+        public static bool IsPalindrome(string s, int left, int right)
+        {
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            ValidPalindrome("abca");
+            ValidPalindrome("abca");
+            ValidPalindrome("abc");
+            ValidPalindrome("deeee");
+        }
+
+        public static bool ValidPalindrome(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                {
+                    return IsPalindrome(s, left + 1, right) || IsPalindrome(s, left, right - 1);
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}

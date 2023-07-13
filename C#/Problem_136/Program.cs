10692e072edcdd46305f22b3551c992ed89627e3
Karthik R <karthik.rcg@zohocorp.com>
2023-07-13 17:52:47 +0530

    Added Problems 136 & 268

---
+++ C#/Problem_136/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_136
+{
+    // 136. Single Number
+    // https://leetcode.com/problems/single-number/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int SingleNumber(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+            foreach (int x in nums)
+            {
+                if (set.Contains(x))
+                {
+                    set.Remove(x);
+                }
+                else
+                {
+                    set.Add(x);
+                }
+            }
+
+            return set.First();
+        }
+    }
+}
---
+++ C#/Problem_268/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Problem_268
+{
+    // 268. Missing Number
+    // https://leetcode.com/problems/missing-number/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 3, 0, 1 };
+            MissingNumber(nums1);
+
+            int[] nums2 = { 0, 1 };
+            MissingNumber(nums2);
+
+            int[] nums3 = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+            MissingNumber(nums3);
+        }
+
+        public static int MissingNumber(int[] nums)
+        {
+            int n = nums.Length;
+            int expected_sum = (n * (n + 1)) / 2;
+            int sum = 0;
+            foreach (int num in nums)
+            {
+                sum += num;
+            }
+
+            return expected_sum - sum;
+        }
+    }
+}

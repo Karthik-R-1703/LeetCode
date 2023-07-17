fe83720230187810a4fb3c2fa0a14e04ae367cc2
Karthik R <karthik.rcg@zohocorp.com>
2023-07-17 19:32:35 +0530

    Added Problems 101 & 104

---
+++ C#/Problem_101/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Problem_101
+{
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 101. Symmetric Tree
+    // https://leetcode.com/problems/symmetric-tree/
+    internal class Program
+    {
+        public static bool CheckSymmetry(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null)
+                return true;
+
+            if (left == null || right == null)
+                return false;
+
+            return CheckSymmetry(left.left, right.right) && CheckSymmetry(left.right, right.left);
+        }
+
+        public static bool IsSymmetric(TreeNode root)
+        {
+            return CheckSymmetry(root.left, root.right);
+        }
+
+        static void Main(string[] args)
+        {
+            TreeNode left = new TreeNode(2, new TreeNode(3), new TreeNode(4));
+            TreeNode right = new TreeNode(2, new TreeNode(4), new TreeNode(3));
+            TreeNode root = new TreeNode(1, left, right);
+            IsSymmetric(root);
+
+            IsSymmetric(new TreeNode(1, new TreeNode(2, null, new TreeNode(3)), new TreeNode(2, null, new TreeNode(3))));
+        }
+    }
+}
---
+++ C#/Problem_104/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Problem_104
+{
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 104. Maximum Depth of Binary Tree
+    // https://leetcode.com/problems/maximum-depth-of-binary-tree/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode right = new TreeNode(20, new TreeNode(15), new TreeNode(7));
+            TreeNode root = new TreeNode(3, new TreeNode(9), right);
+            MaxDepth(root);
+
+            MaxDepth(new TreeNode(1, null, new TreeNode(2)));
+        }
+
+        public static int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int left_depth = MaxDepth(root.left);
+            int right_depth = MaxDepth(root.right);
+
+            return Math.Max(left_depth, right_depth) + 1;
+        }
+    }
+}

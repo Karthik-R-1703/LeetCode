87e33bb7ad5d388c23137b987f6f2933608663ac
Karthik R <karthik.rcg@zohocorp.com>
2023-07-17 16:54:21 +0530

    Added Problems 102 & 144

---
+++ C#/Problem_102/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+
+namespace Problem_102
+{
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 102. Binary Tree Level Order Traversal
+    // https://leetcode.com/problems/binary-tree-level-order-traversal/
+    internal class Program
+    {
+        public static Queue<TreeNode> GetLevel(Queue<TreeNode> levelQ, out List<int> level)
+        {
+            Queue<TreeNode> levelQ2 = new Queue<TreeNode>();
+            level = new List<int>();
+
+            while (levelQ.Count > 0)
+            {
+                TreeNode t1 = levelQ.Dequeue();
+                level.Add(t1.val);
+
+                if (t1.left != null)
+                    levelQ2.Enqueue(t1.left);
+
+                if (t1.right != null)
+                    levelQ2.Enqueue(t1.right);
+            }
+
+            return levelQ2;
+        }
+
+        public static IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            List<IList<int>> LevelValues = new List<IList<int>>();
+
+            if (root == null)
+                return LevelValues;
+
+            Queue<TreeNode> levelQ = new Queue<TreeNode>();
+            levelQ.Enqueue(root);
+
+            Queue<TreeNode> levelQ2 = new Queue<TreeNode>();
+            while (levelQ.Count > 0)
+            {
+                List<int> level = new List<int>();
+                levelQ = GetLevel(levelQ, out level);
+                if (level.Count > 0)
+                {
+                    LevelValues.Add(level);
+                }
+            }
+
+            return LevelValues;
+        }
+
+        static void Main(string[] args)
+        {
+            TreeNode right = new TreeNode(20, new TreeNode(15), new TreeNode(7));
+            TreeNode root = new TreeNode(3, new TreeNode(9), right);
+            LevelOrder(root);
+        }
+    }
+}
---
+++ C#/Problem_144/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Problem_144
+{
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 144. Binary Tree Preorder Traversal
+    // https://leetcode.com/problems/binary-tree-preorder-traversal/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static IList<int> PreorderTraversal(TreeNode root)
+        {
+            if (root == null)
+                return new List<int>();
+
+            List<int> list = new List<int>();
+
+            list.Add(root.val);
+            list.AddRange(PreorderTraversal(root.left));
+            list.AddRange(PreorderTraversal(root.right));
+
+            return list;
+        }
+    }
+}

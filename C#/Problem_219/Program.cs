d6fb1a6649d6edfa26db65ec2c310c3e2a4e3b2d
Karthik R <karthik.rcg@zohocorp.com>
2023-07-19 22:35:25 +0530

    Added Problems 219 & 350

---
+++ C#/Problem_219/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_219
+{
+    // 219. Contains Duplicate II
+    // https://leetcode.com/problems/contains-duplicate-ii/
+    internal class Program
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            Dictionary<int, List<int>> d_nums = new Dictionary<int, List<int>>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (d_nums.ContainsKey(nums[i]))
+                {
+                    d_nums[nums[i]].Add(i);
+                }
+                else
+                {
+                    d_nums.Add(nums[i], new List<int>() { i });
+                }
+            }
+
+            foreach (KeyValuePair<int, List<int>> keyValuePair in d_nums)
+            {
+                for (int x = 0; x < keyValuePair.Value.Count - 1; x++)
+                {
+                    int i = keyValuePair.Value[x];
+                    int j = keyValuePair.Value[x + 1];
+                    if (Math.Abs(i - j) <= k)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ C#/Problem_350/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_350
+{
+    // 350. Intersection of Two Arrays II
+    // https://leetcode.com/problems/intersection-of-two-arrays-ii/
+    internal class Program
+    {
+        public static int[] Intersect(int[] nums1, int[] nums2)
+        {
+            Dictionary<int, int> nums_1 = nums1.GroupBy(x => x).ToDictionary(y => y.Key, y => y.Count());
+            Dictionary<int, int> nums_2 = nums2.GroupBy(x => x).ToDictionary(y => y.Key, y => y.Count());
+            List<int> result = new List<int>();
+            foreach (KeyValuePair<int, int> keyValuePair in nums_1)
+            {
+                if (nums_2.ContainsKey(keyValuePair.Key))
+                {
+                    int value_1 = keyValuePair.Value;
+                    int value_2 = nums_2[keyValuePair.Key];
+                    int min = Math.Min(value_1, value_2);
+                    for (int i = 0; i < min; i++)
+                        result.Add(keyValuePair.Key);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> nums1;
+            List<int> nums2;
+
+            nums1 = new List<int>() { 1, 2, 2, 1 };
+            nums2 = new List<int>() { 2, 2 };
+            Intersect(nums1.ToArray(), nums2.ToArray());
+
+            nums1 = new List<int>() { 4, 9, 5 };
+            nums2 = new List<int>() { 9, 4, 9, 8, 4 };
+            Intersect(nums1.ToArray(), nums2.ToArray());
+        }
+    }
+}

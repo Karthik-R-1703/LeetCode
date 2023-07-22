5cb3addb5c7d917884ffda33de1343c1f3022ed3
Karthik R <karthik.rcg@zohocorp.com>
2023-07-22 12:32:37 +0530

    Add Problems 100 & 169

---
+++ C#/Problem_100/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Problem_100
+{
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 100. Same Tree
+    // https://leetcode.com/problems/same-tree/
+    internal class Program
+    {
+        public static bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+            else if (p == null || q == null)
+                return false;
+
+            bool left = IsSameTree(p.left, q.left);
+
+            if (!left)
+                return false;
+
+            bool right = IsSameTree(p.right, q.right);
+
+            if (!right)
+                return false;
+
+            if (p.val != q.val)
+                return false;
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            TreeNode T1_1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+            TreeNode T1_2 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+            bool ret1 = IsSameTree(T1_1, T1_2);
+
+            TreeNode T2_1 = new TreeNode(1, new TreeNode(2));
+            TreeNode T2_2 = new TreeNode(1, null, new TreeNode(3));
+            bool ret2 = IsSameTree(T2_1, T2_2);
+
+            TreeNode T3_1 = new TreeNode(1, new TreeNode(2), new TreeNode(1));
+            TreeNode T3_2 = new TreeNode(1, new TreeNode(1), new TreeNode(2));
+            bool ret3 = IsSameTree(T3_1, T3_2);
+        }
+    }
+}
---
+++ C#/Problem_169/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Problem_169
+{
+    // 169. Majority Element
+    // https://leetcode.com/problems/majority-element/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[] { 3, 2, 3 };
+            MajorityElement(nums1);
+
+            int[] nums2 = new int[] { 2, 2, 1, 1, 1, 2, 2 };
+            MajorityElement(nums2);
+        }
+
+        public static int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> d_nums = new Dictionary<int, int>();
+            int maxKey = 0;
+            int maxCount = 0;
+            foreach (int num in nums)
+            {
+                if (d_nums.ContainsKey(num))
+                {
+                    d_nums[num]++;
+                }
+                else
+                {
+                    d_nums.Add(num, 1);
+                }
+
+                if (maxCount < d_nums[num])
+                {
+                    maxCount = d_nums[num];
+                    maxKey = num;
+                }
+            }
+
+            return maxKey;
+        }
+    }
+}

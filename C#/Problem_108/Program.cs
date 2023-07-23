2ba0fbd734cfd3a0e3ab54414cc5a013bbf5d5d7
Karthik R <karthik.rcg@zohocorp.com>
2023-07-23 23:02:10 +0530

    Added problem 108 partial add problem 80

---
+++ C#/Problem_108/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Problem_108
+{
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 108. Convert Sorted Array to Binary Search Tree
+    // https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { -10, -3, 0, 5, 9 };
+            TreeNode root1 = SortedArrayToBST(nums1);
+
+            int[] nums2 = { 1, 3 };
+            TreeNode root2 = SortedArrayToBST(nums2);
+        }
+
+        public static TreeNode SortedArrayToBST(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return null;
+
+            int mid = nums.Length / 2;
+
+            TreeNode root = new TreeNode(nums[mid]);
+
+            int[] left = new int[mid];
+            Array.Copy(nums, left, mid);
+            root.left = SortedArrayToBST(left);
+
+            int[] right = new int[nums.Length - 1 - mid];
+            Array.Copy(nums, mid + 1, right, 0, nums.Length - 1 - mid);
+            root.right = SortedArrayToBST(right);
+
+            return root;
+        }
+    }
+}
---
+++ C#/Problem_80/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Problem_80
+{
+    // 80. Remove Duplicates from Sorted Array II
+    // https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 1, 1, 1, 2, 2, 3 };
+            RemoveDuplicates(nums1);
+
+            int[] nums2 = { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+            RemoveDuplicates(nums2);
+        }
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int left = 0, right = 1, count = 1;
+            while (right < nums.Length)
+            {
+                if (nums[left] != nums[right])
+                {
+                    nums[left + right] = nums[right];
+                    count = 1;
+                }
+                else if (nums[left] == nums[right] && count < 2)
+                {
+                    nums[left + right] = nums[right];
+                    count++;
+                }
+
+                right++;
+            }
+
+            return left + 1;
+        }
+    }
+}

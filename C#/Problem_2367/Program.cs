7178c468624527b67ec0231908823eba5bb9eea4
Karthik R <karthik.rcg@zohocorp.com>
2023-07-24 19:51:47 +0530

    Empty add Problem 2367

---
+++ C#/Problem_2367/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Problem_2367
+{
+    // 2367. Number of Arithmetic Triplets
+    // https://leetcode.com/problems/number-of-arithmetic-triplets/
+    internal class Program
+    {
+        public int ArithmeticTriplets(int[] nums, int diff)
+        {
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}

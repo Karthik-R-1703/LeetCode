6e3121e5c6b74188489318b99e550bfcb55e330e
Karthik <karthikr.1703@gmail.com>
2023-07-24 22:51:03 +0530

    Completed Problem 2367

--- C#/Problem_2367/Program.cs
+++ C#/Problem_2367/Program.cs
@@ -1,16 +1,31 @@
-﻿namespace Problem_2367
+﻿using System.Collections.Generic;
+
+namespace Problem_2367
 {
     // 2367. Number of Arithmetic Triplets
     // https://leetcode.com/problems/number-of-arithmetic-triplets/
     internal class Program
     {
-        public int ArithmeticTriplets(int[] nums, int diff)
+        public static int ArithmeticTriplets(int[] nums, int diff)
         {
-            return 0;
+            int count = 0;
+            HashSet<int> triplets = new HashSet<int>(nums);
+            foreach (int i in triplets)
+            {
+                if (triplets.Contains(i - diff) && triplets.Contains(i - (2 * diff)))
+                    count++;
+            }
+
+            return count;
         }
 
         static void Main(string[] args)
         {
+            int[] nums1 = { 0, 1, 4, 6, 7, 10 };
+            ArithmeticTriplets(nums1, 3);
+
+            int[] nums2 = { 4, 5, 6, 7, 8, 9 };
+            ArithmeticTriplets(nums2, 2);
         }
     }
 }

b4374d4d461a36eb817ce0f3a99b2c4db624519d
Karthik <karthikr.1703@gmail.com>
2023-07-26 23:58:45 +0530

    Added Problem 24

---
+++ C#/Problem_24/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Problem_24
+{
+    // Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // 24. Swap Nodes in Pairs
+    // https://leetcode.com/problems/swap-nodes-in-pairs/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            ListNode cur = head;
+            ListNode next = head.next;
+
+            cur.next = SwapPairs(next.next);
+            next.next = cur;
+
+            return next;
+        }
+    }
+}

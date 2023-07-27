85473781bc75c743c045591de70f31bcc92154b8
Karthik <karthikr.1703@gmail.com>
2023-07-27 23:30:48 +0530

    Added Problem 2215

---
+++ C#/Problem_2215/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_2215
+{
+    // 2215. Find the Difference of Two Arrays
+    // https://leetcode.com/problems/find-the-difference-of-two-arrays/
+    internal class Program
+    {
+        public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+        {
+            HashSet<int> nums_1 = new HashSet<int>(nums1);
+            HashSet<int> nums_2 = new HashSet<int>(nums2);
+
+            foreach (int i in nums_1)
+            {
+                if (nums_2.Contains(i))
+                {
+                    nums_1.Remove(i);
+                    nums_2.Remove(i);
+                }
+            }
+
+            List<IList<int>> result = new List<IList<int>>
+            {
+                nums_1.ToList(),
+                nums_2.ToList()
+            };
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}

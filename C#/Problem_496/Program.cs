890d6ae63c9357e5bfa916c2d8b7659b9eae0c44
Karthik <karthikr.1703@gmail.com>
2023-07-30 23:52:34 +0530

    Added Problem 605 Partial add Problem 496

---
+++ C#/Problem_496/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Problem_496
+{
+    // 496. Next Greater Element I
+    // https://leetcode.com/problems/next-greater-element-i/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1_1 = { 4, 1, 2 };
+            int[] nums1_2 = { 1, 3, 4, 2 };
+            NextGreaterElement(nums1_1, nums1_2);
+
+            int[] nums2_1 = { 2, 4 };
+            int[] nums2_2 = { 1, 2, 3, 4 };
+            NextGreaterElement(nums2_1, nums2_2);
+        }
+
+        public static int[] NextGreaterElement(int[] nums1, int[] nums2)
+        {
+            int[] result = new int[nums1.Length];
+            for (int n = 0; n < nums1.Length; n++)
+            {
+                int max = int.MinValue;
+                int i = 0;
+                for (i = nums2.Length - 1; i >= 0; i--)
+                {
+                    if (nums2[i] == nums1[n])
+                        break;
+
+                    if (nums2[i] > max)
+                        max = nums2[i];
+                }
+
+                if (max == int.MinValue)
+                    result[n] = -1;
+                else if (max > nums1[n])
+                    result[n] = i;
+                else
+                    result[n] = -1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C#/Problem_605/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Problem_605
+{
+    // 605. Can Place Flowers
+    // https://leetcode.com/problems/can-place-flowers/
+    internal class Program
+    {
+        public static bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            int count = 0;
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (flowerbed[i] == 0)
+                {
+                    bool emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
+                    bool emptyRightPlot = (i == flowerbed.Length - 1) || (flowerbed[i + 1] == 0);
+
+                    if (emptyLeftPlot && emptyRightPlot)
+                    {
+                        flowerbed[i] = 1;
+                        count++;
+                        if (count >= n)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return count >= n;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] flowerbed = { 1, 0, 0, 0, 1 };
+            CanPlaceFlowers(flowerbed, 1);
+            CanPlaceFlowers(flowerbed, 2);
+        }
+    }
+}

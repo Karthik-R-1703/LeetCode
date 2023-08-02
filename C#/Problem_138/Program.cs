5c1449b3de0af082f363c196534c645e003642e5
Karthik R <karthik.rcg@zohocorp.com>
2023-08-02 19:31:37 +0530

    Empty Add Problem 61 & 138

---
+++ C#/Problem_138/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Problem_138
+{
+    // Definition for a Node.
+    public class Node
+    {
+        public int val;
+        public Node next;
+        public Node random;
+
+        public Node(int _val)
+        {
+            val = _val;
+            next = null;
+            random = null;
+        }
+    }
+
+    // 138. Copy List with Random Pointer
+    // https://leetcode.com/problems/copy-list-with-random-pointer/
+    internal class Program
+    {
+        public static Node CopyRandomList(Node head)
+        {
+            Node copy = new Node(head.val);
+
+            return copy;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ C#/Problem_61/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Problem_61
+{
+    // Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // 61. Rotate List
+    // https://leetcode.com/problems/rotate-list/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static ListNode RotateRight(ListNode head, int k)
+        {
+            return head;
+        }
+    }
+}

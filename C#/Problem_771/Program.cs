c93aad654b0fe918c7df9398af71a2e6d2b0ccad
Karthik <karthikr.1703@gmail.com>
2023-08-02 23:58:32 +0530

    Added Problem 771

---
+++ C#/Problem_771/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Problem_771
+{
+    // 771. Jewels and Stones
+    // https://leetcode.com/problems/jewels-and-stones/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            int count = 0;
+            HashSet<char> set = new HashSet<char>(jewels);
+            foreach (char c in stones)
+            {
+                if (set.Contains(c))
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}

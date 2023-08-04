12c4c6e2db1a53c4d768d992a919970e2f09324d
Karthik R <karthik.rcg@zohocorp.com>
2023-08-04 12:41:44 +0530

    Added Problem 83 Empty Add Problem 652

---
+++ C#/Problem_652/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Problem_652
+{
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 652. Find Duplicate Subtrees
+    // https://leetcode.com/problems/find-duplicate-subtrees/
+    internal class Program
+    {
+        public IList<TreeNode> FindDuplicateSubtrees(TreeNode root)
+        {
+            List<TreeNode> d = new List<TreeNode>();
+            return d;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ C#/Problem_83/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Problem_83
+{
+    // Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // 83. Remove Duplicates from Sorted List
+    // https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+    internal class Program
+    {
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null)
+                return null;
+
+            ListNode cur = head;
+            while (cur != null && cur.next != null)
+            {
+                while (cur.next != null && cur.val == cur.next.val)
+                {
+                    cur.next = cur.next.next;
+                }
+
+                cur = cur.next;
+            }
+
+            return head;
+        }
+
+        static void Main(string[] args)
+        {
+            ListNode head1 = new ListNode(1);
+            head1.next = new ListNode(1);
+            head1.next.next = new ListNode(2);
+            DeleteDuplicates(head1);
+
+            ListNode head2 = new ListNode(1);
+            head2.next = new ListNode(1);
+            head2.next.next = new ListNode(2);
+            head2.next.next.next = new ListNode(3);
+            head2.next.next.next.next = new ListNode(3);
+            DeleteDuplicates(head2);
+        }
+    }
+}

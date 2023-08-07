df7b4fdaacfd5d846426dde30b39faf644cad2c4
Karthik R <karthik.rcg@zohocorp.com>
2023-08-07 16:18:18 +0530

    Added Problem 6 & 8

---
+++ C#/Problem_6/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Problem_6
+{
+    // 6. Zigzag Conversion
+    // https://leetcode.com/problems/zigzag-conversion/
+    internal class Program
+    {
+        public static string Convert(string s, int numRows)
+        {
+            if (numRows == 1)
+                return s;
+
+            string[] result = new string[numRows];
+            int i = 0;
+            bool frwd = true;
+            foreach (char c in s)
+            {
+                if (result[i] == null)
+                    result[i] = string.Empty;
+
+                result[i] += c;
+                if (frwd)
+                {
+                    i++;
+                }
+                else
+                {
+                    i--;
+                }
+
+                if (i == numRows - 1 || i == 0)
+                    frwd = !frwd;
+            }
+
+            return string.Join(string.Empty, result);
+        }
+
+        static void Main(string[] args)
+        {
+            Convert("PAYPALISHIRING", 3);
+            Convert("PAYPALISHIRING", 4);
+            Convert("A", 1);
+            Convert("AB", 1);
+        }
+    }
+}
---
+++ C#/Problem_8/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Problem_8
+{
+    // 8. String to Integer (atoi)
+    // https://leetcode.com/problems/string-to-integer-atoi/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = MyAtoi("42");
+            int num2 = MyAtoi("   -42");
+            int num3 = MyAtoi("4193 with words");
+            int num4 = MyAtoi("words and 987");
+            int num5 = MyAtoi("-91283472332");
+            int num6 = MyAtoi("9223372036854775808");
+            int num7 = MyAtoi(" ++1");
+        }
+
+        public static int MyAtoi(string s)
+        {
+            string s_num = string.Empty;
+            bool isPositive = true;
+
+            s = s.Trim();
+
+            if (s.StartsWith("+"))
+            {
+                s = s.Substring(1);
+            }
+            else if (s.StartsWith("-"))
+            {
+                isPositive = false;
+                s = s.Substring(1);
+            }
+
+            foreach (char c in s)
+            {
+                if (char.IsDigit(c))
+                {
+                    s_num += c;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (int.TryParse(s_num, out int num))
+            {
+                if (!isPositive)
+                {
+                    num *= -1;
+                }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(s_num))
+                    return 0;
+
+                if (isPositive)
+                {
+                    return int.MaxValue;
+                }
+                else
+                {
+                    return int.MinValue;
+                }
+            }
+
+            return num;
+        }
+    }
+}

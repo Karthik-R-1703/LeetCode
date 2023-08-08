d6b35d48592d241735043f90fd745b876747756b
Karthik R <karthik.rcg@zohocorp.com>
2023-08-08 21:35:59 +0530

    Added Problems 33 & 74

---
+++ C#/Problem_33/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Problem_33
+{
+    // 33. Search in Rotated Sorted Array
+    // https://leetcode.com/problems/search-in-rotated-sorted-array/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums_1 = { 4, 5, 6, 7, 0, 1, 2 };
+            Search(nums_1, 0);
+            Search(nums_1, 3);
+
+            int[] nums_2 = { 1 };
+            Search(nums_2, 0);
+        }
+
+        public static int Search(int[] nums, int target)
+        {
+            int start = 0;
+            int end = nums.Length - 1;
+
+            while (start <= end)
+            {
+                int index = (start + end) / 2;
+
+                if (nums[index] == target)
+                {
+                    return index;
+                }
+                else if (nums[start] <= nums[index])
+                {
+                    if (nums[start] <= target && target < nums[index])
+                    {
+                        end = index - 1;
+                    }
+                    else
+                    {
+                        start = index + 1;
+                    }
+                }
+                else
+                {
+                    if (target <= nums[end] && target > nums[index])
+                    {
+                        start = index + 1;
+                    }
+                    else
+                    {
+                        end = index - 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ C#/Problem_74/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Problem_74
+{
+    // 74. Search a 2D Matrix
+    // https://leetcode.com/problems/search-a-2d-matrix/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] matrix_1 = new int[][] {
+                new int[] { 1, 3, 5, 7 },
+                new int[] { 10, 11, 16, 20 },
+                new int[] { 23, 30, 34, 60 }
+            };
+
+            SearchMatrix(matrix_1, 3);
+            SearchMatrix(matrix_1, 13);
+        }
+
+        public static bool SearchMatrix(int[][] matrix, int target)
+        {
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            int left = 0;
+            int right = (m * n) - 1;
+
+            while (left <= right)
+            {
+                int i = left + (right - left) / 2;
+                int temp = matrix[i / n][i % n];
+
+                if (temp == target)
+                    return true;
+                else if (temp < target)
+                    left = i + 1;
+                else
+                    right = i - 1;
+            }
+
+            return false;
+        }
+    }
+}

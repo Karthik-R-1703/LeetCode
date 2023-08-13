5e6d7c9886e6fb9699979c70954009b728898f94
Karthik R <karthik.rcg@zohocorp.com>
2023-08-13 15:19:39 +0530

    Added Problems 1290 & 2181

---
+++ C#/Problem_1290/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Problem_1290
+{
+    // Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // 1290. Convert Binary Number in a Linked List to Integer
+    // https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+    internal class Program
+    {
+        public static int GetDecimalValue(ListNode head)
+        {
+            string binary = string.Empty;
+            while (head != null)
+            {
+                binary += head.val;
+                head = head.next;
+            }
+
+            return Convert.ToInt32(binary, 2);
+        }
+
+        static void Main(string[] args)
+        {
+            ListNode head1 = new ListNode(1);
+            head1.next = new ListNode(0);
+            head1.next.next = new ListNode(1);
+            GetDecimalValue(head1);
+
+            GetDecimalValue(new ListNode(0));
+        }
+    }
+}
---
+++ C#/Problem_2181/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Problem_2181
+{
+    // Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // 2181. Merge Nodes in Between Zeros
+    // https://leetcode.com/problems/merge-nodes-in-between-zeros/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head1 = new ListNode(0);
+            head1.next = new ListNode(3);
+            head1.next.next = new ListNode(1);
+            head1.next.next.next = new ListNode(0);
+            head1.next.next.next.next = new ListNode(4);
+            head1.next.next.next.next.next = new ListNode(5);
+            head1.next.next.next.next.next.next = new ListNode(2);
+            head1.next.next.next.next.next.next.next = new ListNode(0);
+            MergeNodes(head1);
+        }
+
+        public static ListNode MergeNodes(ListNode head)
+        {
+            ListNode result = new ListNode();
+            ListNode prev = result;
+            while (head != null)
+            {
+                while (head != null && head.val == 0)
+                {
+                    head = head.next;
+                }
+
+                if (head != null)
+                {
+                    int num = 0;
+                    while (head != null && head.val != 0)
+                    {
+                        num += head.val;
+                        head = head.next;
+                    }
+
+                    ListNode cur = new ListNode(num);
+                    prev.next = cur;
+                    prev = cur;
+                }
+            }
+
+            return result.next;
+        }
+    }
+}

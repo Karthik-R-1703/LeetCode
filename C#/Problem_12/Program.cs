5c451be78d92ea5807279be738d600d73ee4c9b4
Karthik R <karthik.rcg@zohocorp.com>
2023-08-23 17:38:06 +0530

    Added Problem 12

---
+++ C#/Problem_12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Problem_12
+{
+    // 12. Integer to Roman
+    // https://leetcode.com/problems/integer-to-roman/
+    internal class Program
+    {
+        public static string IntToRoman(int num)
+        {
+            string[] thousands = { "", "M", "MM", "MMM" };
+            string[] hundreds = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] ones = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+            StringBuilder RomanString = new StringBuilder();
+            RomanString.Append(thousands[num / 1000]);
+            num %= 1000;
+            RomanString.Append(hundreds[num / 100]);
+            num %= 100;
+            RomanString.Append(tens[num / 10]);
+            num %= 10;
+            RomanString.Append(ones[num]);
+            return RomanString.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            IntToRoman(3);
+            IntToRoman(58);
+            IntToRoman(1994);
+        }
+    }
+}

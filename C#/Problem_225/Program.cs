1ed45dd4635bea4c03cbf0ba3ae4debdb9a6eb5e
Karthik R <karthik.rcg@zohocorp.com>
2023-08-29 19:19:47 +0530

    Added Problem 225 Partial add Problem 2483

---
+++ C#/Problem_225/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace Problem_225
+{
+    public class MyStack
+    {
+        Queue<int> stack;
+
+        public MyStack()
+        {
+            stack = new Queue<int>();
+        }
+
+        public void Push(int x)
+        {
+            stack.Enqueue(x);
+            int count = stack.Count;
+            while (count > 1)
+            {
+                stack.Enqueue(stack.Dequeue());
+                count--;
+            }
+        }
+
+        public int Pop()
+        {
+            return stack.Dequeue();
+        }
+
+        public int Top()
+        {
+            return stack.Peek();
+        }
+
+        public bool Empty()
+        {
+            return stack.Count == 0;
+        }
+    }
+
+    /**
+     * Your MyStack object will be instantiated and called as such:
+     * MyStack obj = new MyStack();
+     * obj.Push(x);
+     * int param_2 = obj.Pop();
+     * int param_3 = obj.Top();
+     * bool param_4 = obj.Empty();
+     */
+
+    // 225. Implement Stack using Queues
+    // https://leetcode.com/problems/implement-stack-using-queues/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack myStack = new MyStack();
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Top(); // return 2
+            myStack.Pop(); // return 2
+            myStack.Empty(); // return False
+        }
+    }
+}
---
+++ C#/Problem_2483/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+namespace Problem_2483
+{
+    // 2483. Minimum Penalty for a Shop
+    // https://leetcode.com/problems/minimum-penalty-for-a-shop/
+    internal class Program
+    {
+        public static int BestClosingTime(string customers)
+        {
+            int totalYCount = customers.Count(x => x == 'Y');
+            int totalNCount = 0;
+            int minPenalty = totalYCount;
+            int minPenaltyIndex = 0;
+
+            for (int i = 0; i < customers.Length; i++)
+            {
+                if (customers[i] == 'Y')
+                    totalYCount--;
+                else
+                    totalNCount++;
+
+                int curPenalty = totalYCount - totalNCount;
+                if (curPenalty < minPenalty)
+                {
+                    minPenalty = curPenalty;
+                    minPenaltyIndex = i + 1;
+                }
+            }
+
+            return minPenaltyIndex;
+        }
+
+        static void Main(string[] args)
+        {
+            BestClosingTime("YYNY");
+            BestClosingTime("NNNNN");
+            BestClosingTime("YYYY");
+        }
+    }
+}

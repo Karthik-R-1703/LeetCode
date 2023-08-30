9c1d5baa570351b866973faeddd67e809379a40e
Karthik R <karthik.rcg@zohocorp.com>
2023-08-30 21:13:55 +0530

    Completed Problem 61

--- C#/Problem_61/Program.cs
+++ C#/Problem_61/Program.cs
@@ -22,6 +22,36 @@
 
         public static ListNode RotateRight(ListNode head, int k)
         {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            ListNode temp = head;
+            int Length = 0;
+            while (temp != null)
+            {
+                temp = temp.next;
+                Length++;
+            }
+
+            k %= Length;
+
+            while (k > 0)
+            {
+                ListNode cur = head;
+                while (cur.next.next != null)
+                {
+                    cur = cur.next;
+                }
+
+                ListNode last = cur.next;
+                last.next = head;
+                cur.next = null;
+                head = last;
+                k--;
+            }
+
             return head;
         }
     }

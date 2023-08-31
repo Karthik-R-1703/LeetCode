9e6419d123b18151cf18051467f40c4b995def7d
Karthik <karthikr.1703@gmail.com>
2023-08-31 23:22:53 +0530

    Completed Problem 105

--- C#/Problem_105/Program.cs
+++ C#/Problem_105/Program.cs
@@ -20,37 +20,44 @@ namespace Problem_105
     // https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
     internal class Program
     {
-        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        public static TreeNode BuildTree(int[] preorder, int[] inorder)
         {
-            Dictionary<int, int> preOrderMap = new Dictionary<int, int>();
-            for (int i = 0; i < preorder.Length; i++)
+            Dictionary<int, int> inOrderMap = new Dictionary<int, int>();
+            for (int i = 0; i < inorder.Length; i++)
             {
-                preOrderMap.Add(preorder[i], i);
+                inOrderMap.Add(inorder[i], i);
             }
 
-            int inOrderIndex = 0;
-            return BuildTreeNode(preOrderMap, inorder, ref inOrderIndex, 0, preorder.Length - 1);
+            int preOrderIndex = 0;
+            return BuildTreeNode(inOrderMap, preorder, ref preOrderIndex, 0, inorder.Length - 1);
         }
 
-        public static TreeNode BuildTreeNode(Dictionary<int, int> inOrderMap, int[] postOrder, ref int postOrderIndex, int leftIndex, int rightIndex)
+        public static TreeNode BuildTreeNode(Dictionary<int, int> inOrderMap, int[] preOrder, ref int preOrderIndex, int leftIndex, int rightIndex)
         {
             if (leftIndex > rightIndex)
             {
                 return null;
             }
 
-            int root_val = postOrder[postOrderIndex];
+            int root_val = preOrder[preOrderIndex];
             TreeNode root = new TreeNode(root_val);
-            postOrderIndex--;
+            preOrderIndex++;
 
             int Index = inOrderMap[root_val];
-            root.right = BuildTreeNode(inOrderMap, postOrder, ref postOrderIndex, Index + 1, rightIndex);
-            root.left = BuildTreeNode(inOrderMap, postOrder, ref postOrderIndex, leftIndex, Index - 1);
+            root.left = BuildTreeNode(inOrderMap, preOrder, ref preOrderIndex, leftIndex, Index - 1);
+            root.right = BuildTreeNode(inOrderMap, preOrder, ref preOrderIndex, Index + 1, rightIndex);
             return root;
         }
 
         static void Main(string[] args)
         {
+            int[] preorder1 = new int[] { 3, 9, 20, 15, 7 };
+            int[] inorder1 = new int[] { 9, 3, 15, 20, 7 };
+            TreeNode root1 = BuildTree(preorder1, inorder1);
+
+            int[] preorder2 = new int[] { -1 };
+            int[] inorder2 = new int[] { -1 };
+            TreeNode root2 = BuildTree(preorder2, inorder2);
         }
     }
 }

8b13e389364244c50036a1f149936c3f22b1bc46
Karthik <karthikr.1703@gmail.com>
2023-09-13 00:06:27 +0530

    Completed Problems 236 & 297

--- C#/Problem_236/Program.cs
+++ C#/Problem_236/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Problem_236
+﻿using System.Collections.Generic;
+
+namespace Problem_236
 {
     // Definition for a binary tree node.
     public class TreeNode
@@ -13,13 +15,56 @@
     // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
     internal class Program
     {
-        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        public static bool FindTrace(TreeNode root, TreeNode target, Stack<TreeNode> tree)
         {
+            if (root == null)
+                return false;
+
+            tree.Push(root);
+
+            if (root == target)
+                return true;
+
+            if (FindTrace(root.left, target, tree) || FindTrace(root.right, target, tree))
+                return true;
+
+            tree.Pop();
+            return false;
+        }
+
+        public static TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            Stack<TreeNode> pTree = new Stack<TreeNode>();
+            FindTrace(root, p, pTree);
+
+            Stack<TreeNode> qTree = new Stack<TreeNode>();
+            FindTrace(root, q, qTree);
+
+            while (pTree.Count > 0)
+            {
+                TreeNode pNode = pTree.Pop();
+                if (qTree.Contains(pNode))
+                    return pNode;
+            }
+
             return root;
         }
 
         static void Main(string[] args)
         {
+            TreeNode root = new TreeNode(3);
+            root.left = new TreeNode(5);
+            root.right = new TreeNode(1);
+            root.left.left = new TreeNode(6);
+            root.left.right = new TreeNode(2);
+            root.right.left = new TreeNode(0);
+            root.right.right = new TreeNode(8);
+            root.left.right.left = new TreeNode(7);
+            root.left.right.right = new TreeNode(4);
+
+            TreeNode p = root.left;
+            TreeNode q = root.right;
+            LowestCommonAncestor(root, p, q);
         }
     }
 }
--- C#/Problem_297/Program.cs
+++ C#/Problem_297/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Problem_297
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem_297
 {
     // Definition for a binary tree node.
     public class TreeNode
@@ -14,13 +18,46 @@
         // Encodes a tree to a single string.
         public string serialize(TreeNode root)
         {
-            return string.Empty;
+            StringBuilder sb = new StringBuilder();
+            SerializeHelper(root, sb);
+            return sb.ToString();
+        }
+
+        private void SerializeHelper(TreeNode node, StringBuilder sb)
+        {
+            if (node == null)
+            {
+                sb.Append("null,");
+            }
+            else
+            {
+                sb.Append(node.val + ",");
+                SerializeHelper(node.left, sb);
+                SerializeHelper(node.right, sb);
+            }
         }
 
         // Decodes your encoded data to tree.
         public TreeNode deserialize(string data)
         {
-            return null;
+            Queue<string> nodes = new Queue<string>(data.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+            return DeserializeHelper(nodes);
+        }
+
+        private TreeNode DeserializeHelper(Queue<string> nodes)
+        {
+            string val = nodes.Dequeue();
+            if (val == "null")
+            {
+                return null;
+            }
+            else
+            {
+                TreeNode node = new TreeNode(int.Parse(val));
+                node.left = DeserializeHelper(nodes);
+                node.right = DeserializeHelper(nodes);
+                return node;
+            }
         }
     }
 
@@ -35,6 +72,14 @@
     {
         static void Main(string[] args)
         {
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.right.left = new TreeNode(4);
+            root.right.right = new TreeNode(5);
+
+            Codec ser = new Codec();
+            string serializedTree = ser.serialize(root);
         }
     }
 }

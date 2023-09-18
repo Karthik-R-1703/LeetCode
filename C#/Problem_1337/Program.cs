e386b98336265beea8aff917aeb74684ce38db92
Karthik <karthikr.1703@gmail.com>
2023-09-18 23:00:06 +0530

    Completed Problems 1337 & 2129

---
+++ C#/Problem_1337/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+
+namespace Problem_1337
+{
+    // 1337. The K Weakest Rows in a Matrix
+    // https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+    internal class Program
+    {
+        public static int[] KWeakestRows(int[][] mat, int k)
+        {
+            List<int> result = new List<int>(k);
+            Dictionary<int, List<int>> map = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < mat.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < mat[i].Length; j++)
+                {
+                    count += mat[i][j];
+                }
+
+                if (map.ContainsKey(count))
+                {
+                    map[count].Add(i);
+                }
+                else
+                    map.Add(count, new List<int> { i });
+            }
+
+            for (int i = 0; i <= mat[0].Length; i++)
+            {
+                if (map.ContainsKey(i))
+                {
+                    int count = map[i].Count;
+                    if (count <= k)
+                    {
+                        result.AddRange(map[i]);
+                        k -= count;
+                    }
+                    else
+                    {
+                        for (int j = 0; j < mat[0].Length && j < k; j++)
+                        {
+                            result.Add(map[i][j]);
+                        }
+
+                        k = 0;
+                    }
+                }
+
+                if (k == 0)
+                    break;
+            }
+
+            return result.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            int[][] mat1 = new int[][]
+            {
+                new int[] {1, 1, 0, 0, 0},
+                new int[] {1, 1, 1, 1, 0},
+                new int[] {1, 0, 0, 0, 0},
+                new int[] {1, 1, 0, 0, 0},
+                new int[] {1, 1, 1, 1, 1}
+            };
+
+            int[] result1 = KWeakestRows(mat1, 3);
+
+            int[][] mat2 = new int[][]
+            {
+                new int[] {1, 0, 0, 0},
+                new int[] {1, 1, 1, 1},
+                new int[] {1, 0, 0, 0},
+                new int[] {1, 0, 0, 0}
+            };
+
+            int[] result2 = KWeakestRows(mat2, 2);
+
+            int[][] mat3 = new int[][]
+            {
+                new int[] {1, 1, 1, 1, 1, 1},
+                new int[] {1, 1, 1, 1, 1, 1},
+                new int[] {1, 1, 1, 1, 1, 1}
+            };
+
+            int[] result3 = KWeakestRows(mat3, 1);
+        }
+    }
+}
---
+++ C#/Problem_2129/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace Problem_2129
+{
+    // 2129. Capitalize the Title
+    // https://leetcode.com/problems/capitalize-the-title/
+    internal class Program
+    {
+        public static string CapitalizeTitle(string title)
+        {
+            StringBuilder answer = new StringBuilder();
+            string[] words = title.Split(' ');
+            foreach (string word in words)
+            {
+                if (word.Length <= 2)
+                {
+                    answer.Append(word.ToLowerInvariant());
+                }
+                else
+                {
+                    char[] temp = word.ToLowerInvariant().ToCharArray();
+                    temp[0] = char.ToUpper(temp[0]);
+                    answer.Append(new string(temp));
+                }
+
+                answer.Append(" ");
+            }
+
+            return answer.ToString().Trim();
+        }
+
+        static void Main(string[] args)
+        {
+            CapitalizeTitle("capiTalIze tHe titLe");
+            CapitalizeTitle("First leTTeR of EACH Word");
+            CapitalizeTitle("i lOve leetcode");
+        }
+    }
+}

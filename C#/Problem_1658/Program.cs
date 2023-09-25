efd6c52e453a431d9a226d1a0b8f94876ae69ea3
Karthik <karthikr.1703@gmail.com>
2023-09-25 22:56:38 +0530

    Added Problems 1658 & 2824

---
+++ C#/Problem_1658/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_1658
+{
+    // 1658. Minimum Operations to Reduce X to Zero
+    // https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/description/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MinOperations(new int[] { 1, 1, 4, 2, 3 }, 5);
+            MinOperations(new int[] { 5, 6, 7, 8, 9 }, 4);
+            MinOperations(new int[] { 3, 2, 20, 1, 1, 3 }, 10);
+        }
+
+        public static int MinOperations(int[] nums, int x)
+        {
+            int n = nums.Length;
+            int target = nums.Sum() - x;
+
+            if (target == 0)
+                return n;
+
+            Dictionary<int, int> prefixSum = new Dictionary<int, int>();
+            prefixSum[0] = -1;
+            int currentSum = 0;
+            int maxLength = -1;
+
+            for (int i = 0; i < n; i++)
+            {
+                currentSum += nums[i];
+
+                if (prefixSum.ContainsKey(currentSum - target))
+                {
+                    int length = i - prefixSum[currentSum - target];
+                    maxLength = Math.Max(maxLength, length);
+                }
+
+                prefixSum[currentSum] = i;
+            }
+
+            return maxLength == -1 ? -1 : n - maxLength;
+        }
+    }
+}
---
+++ C#/Problem_2824/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Problem_2824
+{
+    // 2824. Count Pairs Whose Sum is Less than Target
+    // https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/description/
+    internal class Program
+    {
+        public static int CountPairs(IList<int> nums, int target)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                for (int j = i + 1; j < nums.Count; j++)
+                {
+                    if (nums[i] + nums[j] < target)
+                        count++;
+                }
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            CountPairs(new List<int> { -1, 1, 2, 3, 1 }, 2);
+            CountPairs(new List<int> { -6, 2, 5, -2, -7, -1, 3 }, -2);
+        }
+    }
+}

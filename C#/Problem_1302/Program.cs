09486b5ab1f55fc907706bd2d92c5bafbc9e8580
Karthik <karthikr.1703@gmail.com>
2023-09-28 21:25:48 +0530

    Added Problem 1302 Added Contest Problem 8048 / 2864

---
+++ C#/Problem_1302/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace Problem_1302
+{
+    // 1302. Deepest Leaves Sum
+    // https://leetcode.com/problems/deepest-leaves-sum/description/
+    internal class Program
+    {
+        public static int DeepestLeavesSum(TreeNode root)
+        {
+            int preSum = root.val;
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                int sum = 0;
+                Queue<TreeNode> level = new Queue<TreeNode>();
+                while (queue.Count > 0)
+                {
+                    TreeNode node = queue.Dequeue();
+
+                    if (node.left != null)
+                    {
+                        sum += node.left.val;
+                        level.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        sum += node.right.val;
+                        level.Enqueue(node.right);
+                    }
+                }
+
+                if (level.Count > 0)
+                {
+                    preSum = sum;
+                    queue = level;
+                }
+            }
+
+            return preSum;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ C#/Problem_8048/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem_8048
+{
+    // 8048. Maximum Odd Binary Number
+    // 2864. Maximum Odd Binary Number
+    // https://leetcode.com/problems/maximum-odd-binary-number/description/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MaximumOddBinaryNumber("010");
+            MaximumOddBinaryNumber("0101");
+        }
+
+        public static string MaximumOddBinaryNumber(string s)
+        {
+            int count = 0;
+            int total = s.Length;
+
+            foreach (char c in s)
+            {
+                if (c == '1')
+                {
+                    count++;
+                }
+            }
+
+            string result = string.Empty;
+
+            result = result.PadLeft(count - 1, '1');
+            result = result.PadRight(total - 1, '0');
+            result = result.PadRight(total, '1');
+            return result;
+        }
+    }
+}

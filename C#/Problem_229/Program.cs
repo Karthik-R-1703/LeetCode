a0522702826783c2121b63deac5b68c2e22b423e
Karthik R <karthik.rcg@zohocorp.com>
2023-10-06 22:26:49 +0530

    Added Problems 229 & 343

---
+++ C#/Problem_229/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_229
+{
+    // 229. Majority Element II
+    // https://leetcode.com/problems/majority-element-ii/description/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MajorityElement(new int[] { 3, 2, 3 });
+            MajorityElement(new int[] { 1 });
+            MajorityElement(new int[] { 1, 2 });
+        }
+
+        public static IList<int> MajorityElement(int[] nums)
+        {
+            int targetCount = nums.Length / 3;
+            return nums.GroupBy(num => num).Where(group => group.Count() > targetCount).Select(group => group.Key).ToList();
+        }
+    }
+}
---
+++ C#/Problem_343/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_343
+{
+    // 343. Integer Break
+    // https://leetcode.com/problems/integer-break/description/
+    internal class Program
+    {
+        public static Dictionary<int, int> maxProduct = new Dictionary<int, int>();
+
+        public static int IntegerBreak(int n)
+        {
+            if (n <= 3)
+                return n - 1;
+
+            return MaxInteger(n);
+        }
+
+        static void Main(string[] args)
+        {
+            IntegerBreak(2);
+            IntegerBreak(10);
+        }
+
+        public static int MaxInteger(int n)
+        {
+            if (n <= 4)
+                return n;
+
+            if (maxProduct.ContainsKey(n))
+                return maxProduct[n];
+
+            int result = n;
+            for (int i = 2; i < n; i++)
+            {
+                result = Math.Max(result, i * MaxInteger(n - i));
+            }
+
+            maxProduct.Add(n, result);
+            return result;
+        }
+    }
+}

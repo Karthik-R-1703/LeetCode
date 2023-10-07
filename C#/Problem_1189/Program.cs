c80060967d408e2455547cf83a667a7e1c02c291
Karthik R <karthik.rcg@zohocorp.com>
2023-10-07 23:52:20 +0530

    Added Problems 290 & 1189

---
+++ C#/Problem_1189/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_1189
+{
+    // 1189. Maximum Number of Balloons
+    // https://leetcode.com/problems/maximum-number-of-balloons/description/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxNumberOfBalloons("nlaebolko");
+            MaxNumberOfBalloons("loonbalxballpoon");
+            MaxNumberOfBalloons("leetcode");
+        }
+
+        public static int MaxNumberOfBalloons(string text)
+        {
+            string comparer = "balloon";
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+            foreach (char c in text)
+            {
+                if (comparer.Contains(c))
+                {
+                    if (charCount.ContainsKey(c))
+                        charCount[c]++;
+                    else
+                        charCount.Add(c, 1);
+                }
+            }
+
+            if (charCount.Count != 5)
+                return 0;
+
+            charCount['l'] /= 2;
+            charCount['o'] /= 2;
+
+            int count = int.MaxValue;
+            foreach (int c in charCount.Values)
+            {
+                count = Math.Min(count, c);
+            }
+
+            return count;
+        }
+    }
+}
---
+++ C#/Problem_290/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_290
+{
+    // 290. Word Pattern
+    // https://leetcode.com/problems/word-pattern/description/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WordPattern("abba", "dog cat cat dog");
+            WordPattern("abba", "dog cat cat fish");
+            WordPattern("aaaa", "dog cat cat dog");
+            WordPattern("abba", "dog dog dog dog");
+            WordPattern("aaa", "aa aa aa aa");
+        }
+
+        public static bool WordPattern(string pattern, string s)
+        {
+            string[] words = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (pattern.Length != words.Length)
+                return false;
+
+            Dictionary<char, string> patternMap = new Dictionary<char, string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (patternMap.ContainsKey(pattern[i]))
+                {
+                    if (patternMap[pattern[i]] != words[i])
+                        return false;
+                }
+                else if (patternMap.ContainsValue(words[i]))
+                    return false;
+                else
+                    patternMap.Add(pattern[i], words[i]);
+            }
+
+            return true;
+        }
+    }
+}

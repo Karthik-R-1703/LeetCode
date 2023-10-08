464440aab64fa21dc89e278cc3a235f05a779a19
Karthik R <karthik.rcg@zohocorp.com>
2023-10-08 23:16:35 +0530

    Added Problems 554 & 1721

---
+++ C#/Problem_1721/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Problem_1721
+{
+    // Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // 1721. Swapping Nodes in a Linked List
+    // https://leetcode.com/problems/swapping-nodes-in-a-linked-list/description/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public ListNode SwapNodes(ListNode head, int k)
+        {
+            List<ListNode> listNodes = new List<ListNode>();
+            ListNode node = head;
+            while (node != null)
+            {
+                listNodes.Add(node);
+                node = node.next;
+            }
+
+            ListNode node_1 = listNodes[k - 1];
+            ListNode node_2 = listNodes[listNodes.Count - k];
+
+            (node_1.val, node_2.val) = (node_2.val, node_1.val);
+
+            return head;
+        }
+    }
+}
---
+++ C#/Problem_554/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_554
+{
+    // 554. Brick Wall
+    // https://leetcode.com/problems/brick-wall/description/
+    internal class Program
+    {
+        public static int LeastBricks(IList<IList<int>> wall)
+        {
+            Dictionary<int, int> rowSum = new Dictionary<int, int>();
+            foreach (var row in wall)
+            {
+                int sum = 0;
+                foreach (var brick in row)
+                {
+                    sum += brick;
+                    if (rowSum.ContainsKey(sum))
+                        rowSum[sum]++;
+                    else
+                        rowSum.Add(sum, 1);
+                }
+            }
+
+            rowSum.Remove(wall[0].Sum());
+            if (rowSum.Count > 0)
+                return wall.Count - rowSum.Values.Max();
+            else
+                return wall.Count;
+        }
+
+        static void Main(string[] args)
+        {
+            IList<IList<int>> wall1 = new List<IList<int>> {
+                new List<int> {1, 2, 2, 1},
+                new List<int> {3, 1, 2},
+                new List<int> {1, 3, 2},
+                new List<int> {2, 4},
+                new List<int> {3, 1, 2},
+                new List<int> {1, 3, 1, 1}
+            };
+
+            LeastBricks(wall1);
+
+            IList<IList<int>> wall2 = new List<IList<int>> {
+                new List<int> {1},
+                new List<int> {1},
+                new List<int> {1}
+            };
+
+            LeastBricks(wall2);
+        }
+    }
+}

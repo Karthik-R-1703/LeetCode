0bfc6cb0b525ba72ba8cc73a8d7ed675b37ab370
Karthik R <karthik.rcg@zohocorp.com>
2023-10-08 09:00:00 +0530

    Added Contest Problems 100085 & 100103

---
+++ C#/Problem_100085/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_100085
+{
+    // 100085. Minimum Processing Time
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MinProcessingTime(new List<int> { 8, 10 }, new List<int> { 2, 2, 3, 1, 8, 7, 4, 5 });
+            MinProcessingTime(new List<int> { 10, 20 }, new List<int> { 2, 3, 1, 2, 5, 8, 4, 3 });
+        }
+
+        public static int MinProcessingTime(IList<int> processorTime, IList<int> tasks)
+        {
+            int[] processorTimeArray = processorTime.ToArray();
+            int[] tasksArray = tasks.ToArray();
+
+            Array.Sort(processorTimeArray);
+            Array.Sort(tasksArray);
+
+            List<int> result = new List<int>();
+            for (int i = 0; i < processorTimeArray.Length; i++)
+            {
+                int t = (processorTimeArray.Length - i) * 4;
+                result.Add(Math.Max(Math.Max(processorTimeArray[i] + tasksArray[t - 1], processorTimeArray[i] + tasksArray[t - 2]), Math.Max(processorTimeArray[i] + tasksArray[t - 3], processorTimeArray[i] + tasksArray[t - 4])));
+            }
+
+            return result.Max();
+        }
+    }
+}
---
+++ C#/Problem_100103/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Problem_100103
+{
+    // 100103. Divisible and Non-divisible Sums Difference
+    internal class Program
+    {
+        public static int DifferenceOfSums(int n, int m)
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % m == 0)
+                    num2 += i;
+                else
+                    num1 += i;
+            }
+
+            return num1 - num2;
+        }
+
+        static void Main(string[] args)
+        {
+            DifferenceOfSums(10, 3);
+            DifferenceOfSums(5, 6);
+            DifferenceOfSums(5, 1);
+        }
+    }
+}

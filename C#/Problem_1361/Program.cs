6b9e6201a7e7461c43be70f42b534eaa64133492
Karthik R <karthik.rcg@zohocorp.com>
2023-10-17 19:24:30 +0530

    Empty Add Problem 1361

---
+++ C#/Problem_1361/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1361
+{
+    // 1361. Validate Binary Tree Nodes
+    //https://leetcode.com/problems/validate-binary-tree-nodes/description/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidateBinaryTreeNodes(4, new int[] { 1, -1, 3, -1 }, new int[] { 2, -1, -1, -1 });
+            ValidateBinaryTreeNodes(4, new int[] { 1, -1, 3, -1 }, new int[] { 2, 3, -1, -1 });
+            ValidateBinaryTreeNodes(2, new int[] { 1, 0 }, new int[] { -1, -1 });
+        }
+        public static bool ValidateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild)
+        {
+            return true;
+        }
+    }
+}

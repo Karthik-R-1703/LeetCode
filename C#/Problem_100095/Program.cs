526c4491fc992a95559d3c1ba40a4a7782a7f0f0
Karthik R <karthik.rcg@zohocorp.com>
2023-10-17 19:14:44 +0530

    Added Contest Problem 100095 / 2899

---
+++ C#/Problem_100095/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Problem_100095
+{
+    // 100095. Last Visited Integers
+    // 2899. Last Visited Integers
+    // https://leetcode.com/problems/last-visited-integers/description/
+    internal class Program
+    {
+        public static IList<int> LastVisitedIntegers(IList<string> words)
+        {
+            List<int> result = new List<int>();
+            List<int> nums = new List<int>();
+            int i = -1;
+            foreach (string word in words)
+            {
+                if (word.Equals("prev"))
+                {
+                    if (i > -1)
+                    {
+                        result.Add(nums[i]);
+                        i--;
+                    }
+                    else
+                    {
+                        result.Add(-1);
+                    }
+                }
+                else
+                {
+                    nums.Add(int.Parse(word));
+                    i = nums.Count - 1;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            LastVisitedIntegers(new List<string> { "1", "2", "prev", "prev", "prev" });
+            LastVisitedIntegers(new List<string> { "1", "prev", "2", "prev", "prev" });
+        }
+    }
+}

79074b870e00c1a720f48c2cc9ceb21165b4450e
Karthik R <karthik.rcg@zohocorp.com>
2024-11-17 17:25:04 +0530

    Partially Added Problem 1574

---
+++ C#/Problem_1574/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Problem_1574
+{
+    // 1574. Shortest Subarray to be Removed to Make Array Sorted
+    // https://leetcode.com/problems/shortest-subarray-to-be-removed-to-make-array-sorted/description/
+    internal class Program
+    {
+        public static int FindLengthOfShortestSubarray(int[] arr)
+        {
+            int r = arr.Length - 1;
+            while (r > 0 && arr[r] >= arr[r - 1])
+                r--;
+
+            int minLength = r;
+            int l = 0;
+            while (l < r - 1 && (l == 0 || arr[l] <= arr[l + 1]))
+            {
+                while (r < arr.Length && arr[l] > arr[r])
+                    r++;
+
+                minLength = Math.Min(minLength, r - l - 1);
+                l++;
+            }
+
+            return minLength;
+        }
+
+        static void Main(string[] args)
+        {
+            FindLengthOfShortestSubarray(new int[] { 1, 2, 3, 10, 4, 2, 3, 5 });
+            FindLengthOfShortestSubarray(new int[] { 2, 2, 2, 1, 1, 1 });
+            FindLengthOfShortestSubarray(new int[] { 6, 3, 10, 11, 15, 20, 13, 3, 18, 12 });
+        }
+    }
+}

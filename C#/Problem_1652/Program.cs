92eb2894add58696e00534b75a418ab66496c2f0
Karthik R <karthik.rcg@zohocorp.com>
2024-11-21 09:12:19 +0530

    Added Problem 1652

---
+++ C#/Problem_1652/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Problem_1652
+{
+    // 1652. Defuse the Bomb
+    // https://leetcode.com/problems/defuse-the-bomb/description/
+    internal class Program
+    {
+        public static int[] Decrypt(int[] code, int k)
+        {
+            int n = code.Length;
+            int[] decryptedCode = new int[n];
+            if (k == 0)
+                return decryptedCode;
+
+            for (int i = 0; i < n; i++)
+            {
+                int start;
+                int end;
+                if (k > 0)
+                {
+                    start = i + 1;
+                    end = i + k + 1;
+                    while (start < end)
+                    {
+                        decryptedCode[i] += code[start % n];
+                        start++;
+                    }
+                }
+                else
+                {
+                    start = i - Math.Abs(k);
+                    end = i;
+                    while (start < end)
+                    {
+                        decryptedCode[i] += code[(start + n) % n];
+                        start++;
+                    }
+                }
+            }
+
+            return decryptedCode;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}

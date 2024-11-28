22264b29f0e83ba05da21ce1d65a8bfd42e0481d
Karthik R <karthik.rcg@zohocorp.com>
2024-11-28 14:09:57 +0530

    Empty add problem 2290

---
+++ C#/Problem_2290/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2290
+{
+    // 2290. Minimum Obstacle Removal to Reach Corner
+    // https://leetcode.com/problems/minimum-obstacle-removal-to-reach-corner/description/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int MinimumObstacles(int[][] grid)
+        {
+            return 0;
+        }
+    }
+}

bd6dd2538e2d6bae2e7e9861394e25ba7a36f020
Karthik R <karthikr.1703@gmail.com>
2024-12-02 08:48:58 +0530

    Added Problem 1455

---
+++ C#/Problem_1455/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem_1455
+{
+    // 1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence
+    // https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/description/
+    internal class Program
+    {
+        public int IsPrefixOfWord(string sentence, string searchWord)
+        {
+            string[] words = sentence.Split(new char[] { ' ' });
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].StartsWith(searchWord, StringComparison.OrdinalIgnoreCase))
+                    return i + 1;
+            }
+
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- C#/Problem_2290/Program.cs
+++ C#/Problem_2290/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Problem_2290
 {
@@ -10,13 +6,103 @@ namespace Problem_2290
     // https://leetcode.com/problems/minimum-obstacle-removal-to-reach-corner/description/
     internal class Program
     {
+        private static int[,] directions = {
+            { 0, 1 },
+            { 0, -1 },
+            { 1, 0 },
+            { -1, 0 },
+        };
+
         static void Main(string[] args)
         {
+            int[][] grid1 = new int[][]
+            {
+                new int[] { 0, 1, 1 },
+                new int[] { 1, 1, 0 },
+                new int[] { 1, 1, 0 }
+            };
+
+            MinimumObstacles(grid1);
         }
 
         public static int MinimumObstacles(int[][] grid)
         {
-            return 0;
+            int m = grid.Length;
+            int n = grid[0].Length;
+            int[,] minObstacles = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    minObstacles[i, j] = int.MaxValue;
+                }
+            }
+
+            minObstacles[0, 0] = 0;
+            Queue<(int, int, int)> zeroQueue = new Queue<(int, int, int)>();
+            zeroQueue.Enqueue((0, 0, 0));
+
+            Queue<(int, int, int)> oneQueue = new Queue<(int, int, int)>();
+
+            while (zeroQueue.Count > 0 || oneQueue.Count > 0)
+            {
+                while (zeroQueue.Count > 0)
+                {
+                    List<Queue<(int, int, int)>> lists = Obstacles(grid, minObstacles, zeroQueue);
+                    zeroQueue = lists[0];
+                    oneQueue = lists[1];
+                }
+
+                while (oneQueue.Count > 0)
+                {
+                    List<Queue<(int, int, int)>> lists = Obstacles(grid, minObstacles, oneQueue);
+                    zeroQueue = lists[0];
+                    oneQueue = lists[1];
+                }
+            }
+
+            return minObstacles[m - 1, n - 1];
+        }
+
+        private static List<Queue<(int, int, int)>> Obstacles(int[][] grid, int[,] minObstacles, Queue<(int, int, int)> curQueue)
+        {
+            int m = grid.Length;
+            int n = grid[0].Length;
+            Queue<(int, int, int)> zeroQueue = new Queue<(int, int, int)>();
+            Queue<(int, int, int)> oneQueue = new Queue<(int, int, int)>();
+            while (curQueue.Count > 0)
+            {
+                (int obstacles, int row, int col) value = curQueue.Dequeue();
+                for (int i = 0; i < 4; i++)
+                {
+                    int newRow = value.row + directions[i, 0];
+                    int newCol = value.col + directions[i, 1];
+                    if (newRow >= 0 && newCol >= 0 && newRow < m && newCol < n)
+                    {
+                        if (minObstacles[newRow, newCol] == int.MaxValue)
+                        {
+                            if (grid[newRow][newCol] == 1)
+                            {
+                                minObstacles[newRow, newCol] = value.obstacles + 1;
+                                oneQueue.Enqueue((value.obstacles + 1, newRow, newCol));
+                            }
+                            else
+                            {
+                                minObstacles[newRow, newCol] = value.obstacles;
+                                zeroQueue.Enqueue((value.obstacles, newRow, newCol));
+                            }
+                        }
+                    }
+                }
+            }
+
+            List<Queue<(int, int, int)>> lists = new List<Queue<(int, int, int)>>
+            {
+                zeroQueue,
+                oneQueue
+            };
+
+            return lists;
         }
     }
 }
